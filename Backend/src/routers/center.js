4ab19fbc038789de423eecc00117cf6d46461b34
pavit1809 <pavitragoyal@hotmail.com>
2021-02-28 07:13:50 +0530

    13.0
    Backend Part almost completed

--- Backend/src/routers/center.js
+++ Backend/src/routers/center.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const Center=require('../models/center');
+const User=require('../models/user');
 const Facility=require('../models/facilities');
+const Appointment=require('../models/appointment');
 const router=new express.Router();
 const RegistrationUtil=require('../helpers/center-registration-helper');
 const RegistrationUtil1=require('../helpers/Registration-helper');
+const AppointmentHelper=require('../helpers/Appointment-helper');
 const Vonage = require('@vonage/server-sdk');
 const nodemailer=require('nodemailer');
 const axios = require('axios').default;
@@ -212,5 +215,273 @@ router.post('/center/prevapp',Authmiddleware,async(req,res)=>{
             res.status(400).send(err);
       }
 })
+
+//Route-7
+router.post('/center/prevapp',Authmiddleware,async(req,res)=>{
+      try{
+            const appointments=await Appointment.find({center_id:req.body.centerInfo.data.center._id,Attended:true});
+            const filtered=AppointmentHelper.arrange(appointments);
+            let ret=[];
+            for(let i=0;i<filtered.length;i++){
+                  const currentuser=await User.findOne({_id:filtered[i].user_id});
+                  ret.push({
+                        Name:currentuser.UserName,
+                        Test:filtered[i].facilityused,
+                        Date:filtered[i].dateofappointment,
+                        Slot:filtered[i].Slotdetails,
+                        PhoneNo:currentuser.PhoneNumber,
+                        Email:currentuser.Email,
+                        appid:filtered[i]._id,
+                        userid:currentuser._id,
+                        flag:((filtered[i].ResultStatus==false)?1:0)
+                  });
+            }
+            // console.log(ret);
+            res.status(200).send(ret);
+      }catch(err){
+            console.log(err);
+            res.status(400).send(err);
+      }
+})
+
+//Route-8
+router.post('/center/presapp',Authmiddleware,async(req,res)=>{
+      try{
+            const appointments=await Appointment.find({center_id:req.body.centerInfo.data.center._id});
+            // console.log(appointments);
+            const filtered=AppointmentHelper.arrange1(appointments);
+            // console.log(filtered);
+            let ret=[];
+            for(let i=0;i<filtered.length;i++){
+                  const currentuser=await User.findOne({_id:filtered[i].user_id});
+                  ret.push({
+                        Name:currentuser.UserName,
+                        Test:filtered[i].facilityused,
+                        Date:filtered[i].dateofappointment,
+                        Slot:filtered[i].Slotdetails,
+                        PhoneNo:currentuser.PhoneNumber,
+                        Email:currentuser.Email,
+                        userid:currentuser._id,
+                        appid:filtered[i]._id,
+                        flag:((filtered[i].Attended==false)?1:0)
+                  });
+            }
+            // console.log(ret);
+            res.status(200).send(ret);
+      }catch(err){
+            console.log(err);
+            res.status(400).send(err);
+      }
+})
+
+//Route-9
+router.post('/center/futapp',Authmiddleware,async (req,res)=>{
+      try{  
+            const appointments=await Appointment.find({center_id:req.body.centerInfo.data.center._id});
+            const filtered=AppointmentHelper.arrange2(appointments);
+            let ret=[];
+            for(let i=0;i<filtered.length;i++){
+                  const currentuser=await User.findOne({_id:filtered[i].user_id});
+                  ret.push({
+                        Name:currentuser.UserName,
+                        Test:filtered[i].facilityused,
+                        date:filtered[i].dateofappointment,
+                        Slot:filtered[i].Slotdetails,
+                        PhoneNo:currentuser.PhoneNumber,
+                        Email:currentuser.Email,
+                        appid:filtered[i]._id,
+                  });
+            }
+            // console.log(ret);
+            res.status(200).send(ret);
+      }catch(err){
+            console.log(err);
+            res.status(400).send(err);
+      }
+})
+
+//Route-10
+router.post('/center/sendcancelmail',Authmiddleware,async (req,res)=>{
+      try{
+            const mailbody=AppointmentHelper.EmailBody(req.body.appInfo.Email,req.body.appInfo.Name,req.body.reason,req.body.centerInfo.data.center.Name,req.body.appInfo.Slot,req.body.appInfo.date,req.body.appInfo.Test);
+            let emailinfo=await transporter.sendMail(mailbody);
+            await Appointment.deleteOne({_id:req.body.appInfo.appid});
+            res.status(200).send();
+      }catch(err){
+            console.log(err);
+            res.status(400).send('Some error occured,Please try again later!');
+      }
+})
+
+//Route-11
+router.post('/center/userverify',Authmiddleware,async (req,res)=>{
+      try{
+            const user=await User.findOne({_id:req.body.userid});
+            // console.log(user.RecentMobileOtps[user.RecentMobileOtps.length-1]);
+            // console.log(req.body.otp);
+            if (user.RecentMobileOtps[user.RecentMobileOtps.length-1]==req.body.otp && user.IdentificationIdNumber==req.body.idnum){
+                  await user.RecentMobileOtps.pop();
+                  const appoint=await Appointment.findOne({_id:req.body.appid});
+                  appoint.Attended=true;
+                  await user.save();
+                  await appoint.save();
+                  res.status(200).send();
+            }
+            else{
+                  res.status(400).send("Data is invalid");
+            }
+      }catch(err){
+            console.log(err);
+            res.status(400).send("Data is invalid");
+      }
+})
+
+//Route-12
+router.post('/center/logout',Authmiddleware,async (req,res)=>{
+      try{
+            req.center.tokens=[];
+            req.center.RecentEmailOtps=[];
+            req.center.RecentMobileOtps=[];
+            await req.center.save();
+            res.status(200).send();
+      }catch(err){
+            console.log(err);
+            res.status(400).send(err);
+      }
+})
+
+//Route-13
+router.post('/center/sendotp',Authmiddleware,async(req,res)=>{
+      // console.log(req.body);
+      try{
+            const center=await Center.findOne({_id:req.body.id});
+            const otp=RegistrationUtil1.GetOtp();
+            if (parseInt(req.body.flag)==0){     
+                  const emailbody=RegistrationUtil1.EmailBody(req.body.value,otp);
+                  // let emailinfo=await transporter.sendMail(emailbody);
+                  center.RecentEmailOtps.push(otp);
+                  await center.save();
+                  res.status(200).send("Otp sent successfully");
+            }
+            else{
+                  const messagebody=RegistrationUtil.MessageBody(otp);
+                  // let messageinfo=await vonage.message.sendSms('Team',"91"+user.PhoneNumber,messagebody);
+                  center.RecentMobileOtps.push(otp);
+                  await center.save();
+                  res.status(200).send("Otp sent successfully");
+            }
+      }catch(err){
+            console.log(err);
+            res.send(400).send(err);
+      }
+})
+
+//Route-14
+router.post('/center/verifyotponupd',Authmiddleware,async (req,res)=>{
+      try{
+            const center=await Center.findOne({_id:req.body.id});
+            if (parseInt(req.body.flag)==0){
+                  if (center.RecentEmailOtps[center.RecentEmailOtps.length-1]==req.body.otp){
+                        res.status(200).send('Verified');
+                  }
+                  else{
+                        res.status(400).send('Invalid Otp');
+                  }
+            }
+            else{
+                  if (center.RecentMobileOtps[center.RecentMobileOtps.length-1]==req.body.otp){
+                        res.status(200).send('Verified');
+                  }
+                  else{
+                        res.status(400).send('Invalid Otp');
+                  }
+            }
+      }catch(err){
+            console.log(err);
+            res.status(400).send(err);
+      }
+})
+
+//Route-15
+router.post('/center/profile',Authmiddleware,async(req,res)=>{
+      try{
+            const facilities=await Facility.find({owner:req.center._id});
+            // console.log
+            res.status(200).send(facilities);
+      }catch(err){
+            //Mostly due to invalid address
+            console.log(err);
+            res.status(400).send(err);
+      }
+})
+
+router.post('/center/sendres',Authmiddleware,async(req,res)=>{
+      try{
+            const appointment=await Appointment.findOneAndUpdate({_id:req.body.appInfo.appid},{ResultStatus:true});
+            res.status(200).send();
+      }catch(err){
+            console.log(err);
+            res.status(400).send();
+      }
+})
+
+router.post('/center/update',Authmiddleware,async (req,res)=>{
+      console.log(req.body);
+      res.status(200).send();
+})
+
+router.post('/center/reviewdet',Authmiddleware,async (req,res)=>{
+      try{
+            const center=await Center.findOne({_id:req.body.centerInfo.data.center._id});
+            if (center.Reviews.length==0){
+                  const reviews={
+                        arr:[{
+                              text:"Good boi",
+                              stars:5
+                        }],
+                        posper:70,
+                        negper:30,
+                        comment:"Everything looks not good as of now",
+                        avgstars:5,
+                        flag:1
+                  };
+                  res.status(200).send(reviews);
+            }
+            else{
+                  let sum=0;
+                  let posp=0,negp=0;
+                  for(let i=0;i<center.Reviews.length;i++){
+                        const data1={
+                              review:center.Reviews[i].text
+                        };
+                        sum+=center.Reviews[i].stars;
+                        const response=await axios.post('http://f7c9e5c63ef0.ngrok.io',data1);
+                        if (response.data=='negative'){
+                              negp+=1;
+                        }else{
+                              posp+=1;
+                        }
+                  }
+                  let comm="hello",flag=1;
+                  if (negp>=posp || parseInt(sum/center.Reviews.length)<=3){
+                        comm='Some Serious Steps are immediately needed',
+                        flag=1;
+                  }else{
+                        comm='Things are going fine'
+                  }
+                  const reviews={
+                        arr:center.Reviews,
+                        posper:parseFloat((posp/(posp+negp))*100),
+                        negper:parseFloat((negp/(posp+negp))*100),
+                        comment:comm,
+                        avgstars:parseFloat(sum/center.Reviews.length)
+                  }
+                  res.status(200).send(reviews);      
+            }
+      }catch{
+
+      }
+})
+
 
 module.exports=router;
